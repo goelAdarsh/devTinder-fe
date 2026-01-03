9c0881c3711a0c5c557484a759430377e9cded95
adarsh-goel-tw <adarsh.goel@thoughtworks.com>
2026-01-03 14:29:32 +0530

    feat: if token is missing from the cookies, logout the user, else keep the user logged in (even on refresh)

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,8 +1,34 @@
-import { Outlet } from "react-router";
+import axios from "axios";
+import { AxiosError } from "axios";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { addUserProfile } from "../store/userSlice";
+import { BASE_URL } from "../constants";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUserProfile = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/profile`, {
+        withCredentials: true,
+      });
+      dispatch(addUserProfile(response.data));
+    } catch (error) {
+      if (error instanceof AxiosError && error.status === 401) {
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+
   return (
     <div>
       <NavBar />
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,42 +1,49 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router";
+
 const NavBar = () => {
+  const user = useSelector((store) => store.user);
+  console.log("NavBar User:", user);
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          DevTinder
+        </Link>
       </div>
-      <div className="flex-none">
-        <div className="dropdown dropdown-end px-4">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
+      {user && (
+        <div className="flex justify-center items-center">
+          <p>Welcome {user?.firstName}!</p>
+          <div className="dropdown dropdown-end px-4">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="User Pic" src={user?.photoUrl} />
+              </div>
             </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={-1}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
         </div>
-      </div>
+      )}
     </div>
   );
 };
